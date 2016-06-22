f9117a0b5c70ca749d6db7e9a1150e15df74694f
MikasaNT <waiyan01101994@gmail.com>
2016-06-22 12:13:58 +0800

    jkl

--- hello/views.py
+++ hello/views.py
@@ -9,9 +9,10 @@ def index(request):
     return render(request, 'index.html')
 
 def hello(request):
-    r = requests.get('http://httpbin.org/status/418')
-    print r.text
-    return HttpResponse('<pre>' + r.text + '</pre>')
+#     r = requests.get('http://httpbin.org/status/418')
+#     print r.text
+#     return HttpResponse('<pre>' + r.text + '</pre>')
+    return HttpResponse("hello world lmao")
 
 def db(request):
 
