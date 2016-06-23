e21c942b513e210ffac17234e71a1aed0439797b
MikasaNT <waiyan01101994@gmail.com>
2016-06-23 12:43:36 +0800

    haha

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -78,8 +78,13 @@ WSGI_APPLICATION = 'gettingstarted.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'capstone',
+        'USER': 'calvinlee708',
+        'PASSWORD': 'chwb5278',
+        'HOST': 'capstoneskyeye.cfyrhe0diz6p.us-west-2.rds.amazonaws.com',
+        'PORT': '3306',
+        
     }
 }
 
