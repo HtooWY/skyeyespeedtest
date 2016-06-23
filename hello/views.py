2869873b8d480975b901d49e8947201c7f737124
MikasaNT <waiyan01101994@gmail.com>
2016-06-23 13:34:18 +0800

    lks

--- hello/views.py
+++ hello/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, render_to_response
 from django.http import HttpResponse
 import requests
 from .models import Greeting
+import MySQLdb as mdb
 
 # Create your views here.
 def index(request):
@@ -9,10 +10,23 @@ def index(request):
     return render(request, 'index.html')
 
 def hello(request):
-#     r = requests.get('http://httpbin.org/status/418')
-#     print r.text
-#     return HttpResponse('<pre>' + r.text + '</pre>')
-    return HttpResponse("hello world lmao")
+    
+    conn=mdb.connect(host='capstoneskyeye.cfyrhe0diz6p.us-west-2.rds.amazonaws.com',port='3306',user='calvinlee708', passwd='chwb5278',db='capstone')
+           
+    with conn:
+        cursor=conn.cursor()
+        cursor.execute("select * from capstonespeedtest;")
+        l=cursor.fetchall()
+        
+#         if cursor.rowcount==0:
+#             raise Http404("Player not found")
+#         else:
+#             row = cursor.fetchone()
+#             context={'pid':row[0]}
+#     if "submit" in request.GET:
+#         if request.GET["submit"]=="get":
+#             return redirect('/player/%s'%(row[0]))      
+    return render_to_response(l)
 
 def db(request):
 
